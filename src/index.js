0e44288b7bb3b3b2a946240d2c5c497d19b8af84
Aseni <alusinejalloh800@gmail.com>
2022-09-15 13:51:32 +0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
-// Components files
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById('root'));
